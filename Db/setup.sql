b074138a9896c21efdb109d1a5f252d1f18cf822
tuong1008 <tuong100852@gmail.com>
2022-05-09 14:49:02 +0700

    add docker

---
+++ Db/setup.sql
@@ -0,0 +1,10 @@
+USE NGANHANG
+GO
+EXEC SP_TAOLOGIN 'adminCN1', 'Admin1234.', '0000000000', 'ChiNhanh'
+GO
+EXEC SP_TAOLOGIN 'adminNH1', 'Admin1234.', '0000000001', 'NganHang'
+GO
+EXEC SP_TAOLOGIN 'adminCN2', 'Admin1234.', '0000000002', 'ChiNhanh'
+GO
+EXEC SP_TAOLOGIN 'adminNH2', 'Admin1234.', '0000000003', 'NganHang'
+GO
