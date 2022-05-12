9bb447601b6bc7b6a7504d06759f8d1db1cb8676
tuong1008 <tuong100852@gmail.com>
2022-05-12 15:34:09 +0700

    update db

--- Db/setup.sql
+++ Db/setup.sql
@@ -8,3 +8,8 @@ EXEC SP_TAOLOGIN 'adminCN2', 'Admin1234.', '0000000002', 'ChiNhanh'
 GO
 EXEC SP_TAOLOGIN 'adminNH2', 'Admin1234.', '0000000003', 'NganHang'
 GO
+EXEC sp_addsrvrolemember 'adminCN1', 'SecurityAdmin'
+EXEC sp_addsrvrolemember 'adminCN1', 'ProcessAdmin'
+GO
+EXEC sp_addsrvrolemember 'adminCN2', 'SecurityAdmin'
+EXEC sp_addsrvrolemember 'adminCN2', 'ProcessAdmin'
--- src/main/java/controller/api/TestConnDBAPi.java
+++ src/main/java/controller/api/TestConnDBAPi.java
@@ -7,10 +7,13 @@ package controller.api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dao.impl.AbstractDAO;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import javax.json.Json;
 import javax.json.stream.JsonGenerator;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,22 +29,46 @@ import javax.servlet.http.HttpSession;
 public class TestConnDBAPi extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setContentType("application/json");
+        // request.setCharacterEncoding("UTF-8");
+        // response.setContentType("application/json");
         
-        Connection connection = AbstractDAO.getConnection("sa", "Admin1234.");
+        // Connection connection = AbstractDAO.getConnection("sa", "Admin1234.");
 
-        String message = "";
-        if (connection != null){
-            message = "Ket noi DB thanh cong";
-        } else {
-            message = "Ket noi DB that bai";
+        // String message = "";
+        // if (connection != null){
+        //     message = "Ket noi DB thanh cong";
+        // } else {
+        //     message = "Ket noi DB that bai";
+        // }
+
+        // JsonGenerator generator = Json.createGenerator(response.getOutputStream());
+        //     generator.writeStartObject()
+        //             .write("message", message)
+        //             .writeEnd();
+        //     generator.close();
+        ServletContext sc = getServletContext();
+
+        try (InputStream is = sc.getResourceAsStream("/images/duck.jpg")) {
+
+            // it is the responsibility of the container to close output stream
+            OutputStream os = response.getOutputStream();
+
+            if (is == null) {
+
+                response.setContentType("text/plain");
+                os.write("Failed to send image".getBytes());
+            } else {
+
+                response.setContentType("image/jpeg");
+
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                while ((bytesRead = is.read(buffer)) != -1) {
+
+                    os.write(buffer, 0, bytesRead);
+                }
+            }
         }
-
-        JsonGenerator generator = Json.createGenerator(response.getOutputStream());
-            generator.writeStartObject()
-                    .write("message", message)
-                    .writeEnd();
-            generator.close();
     }
 }
