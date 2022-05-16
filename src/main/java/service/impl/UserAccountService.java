7610005a86ff9356f761447f5845b6fdb2ec26c2
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2022-05-16 23:03:52 +0700

    Reformat code, xoa bot TODO

--- src/main/java/service/impl/UserAccountService.java
+++ src/main/java/service/impl/UserAccountService.java
@@ -14,19 +14,16 @@ public class UserAccountService implements IUserAccountService {
 
     @Override
     public UserAccount login(HttpServletRequest req, String user, String password) {
-        // TODO Auto-generated method stub
         return userAccountDAO.login(req, user, password);
     }
 
     @Override
     public UserAccount getOne(HttpServletRequest req, String user) {
-        // TODO Auto-generated method stub
         return userAccountDAO.getOne(req, user);
     }
 
     @Override
     public String insert(HttpServletRequest req, String userName, String password, String image, String khachHangID) {
-        // TODO Auto-generated method stub
         return userAccountDAO.insert(req, userName, password, image, khachHangID);
     }
 
@@ -38,8 +35,6 @@ public class UserAccountService implements IUserAccountService {
 
     @Override
     public String updateImage(HttpServletRequest req, String imageUrl, String userId) {
-        // TODO Auto-generated method stub
         return userAccountDAO.updateImage(req, imageUrl, userId);
     }
-
 }
